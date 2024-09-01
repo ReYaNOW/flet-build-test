81c1dd27371624dee71bf7104e19c884c2537915
Sergei <reyangood@gmail.com>
2024-09-01 20:54:49 +0300

    change files

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from flet import *
 import httpx
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    name: str
+    age: int
 
 
 def main(page: Page):
+    user = User(name='name', age='13')
     page.theme_mode = ThemeMode.LIGHT
     r = httpx.get('https://www.google.com/')
-    page.appbar = AppBar(
-        title=Text(f'{r.url}', color="white"), bgcolor="blue"
-    )
+    page.appbar = AppBar(title=Text(f'{r.url}', color="white"), bgcolor="blue")
     page.add(
         Column(
             [
                 Text(f'{r.status_code}'),
-                ElevatedButton("test123", bgcolor="blue", color="white"),
+                ElevatedButton(
+                    f'{user.name}, {user.age}', bgcolor="blue", color="white"
+                ),
             ]
         )
     )
