b07f1e27286a59b9d3b94c08bbb308e778e5ae1c
Sergei <reyangood@gmail.com>
2024-09-01 17:59:43 +0300

    change files

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import flet as fp
-import httpx
 
 
 def main(page: fp.Page):
     page.theme_mode = fp.ThemeMode.LIGHT
-    r = httpx.get('https://www.google.com/')
     page.appbar = fp.AppBar(
-        title=fp.Text(f'{r.url}{r.status_code}', color='white'), bgcolor='blue'
+        title=fp.Text("hello youtube", color="white"), bgcolor="blue"
     )
     page.add(
         fp.Column(
             [
-                fp.Text(f'{r.url}{r.status_code}'),
-                fp.ElevatedButton('test123', bgcolor='blue', color='white'),
+                fp.Text("hello"),
+                fp.ElevatedButton("test123", bgcolor="blue", color="white"),
             ]
         )
     )
