74bfe75b3666fd61da1971d6068191099cf0a849
Sergei <reyangood@gmail.com>
2024-09-01 20:40:50 +0300

    change files

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flet import *
+import httpx
 
 
 def main(page: Page):
     page.theme_mode = ThemeMode.LIGHT
+    r = httpx.get('https://www.google.com/')
     page.appbar = AppBar(
-        title=Text("hello youtube", color="white"), bgcolor="blue"
+        title=Text(f'{r.url}', color="white"), bgcolor="blue"
     )
     page.add(
         Column(
             [
-                Text("hello"),
+                Text(f'{r.status_code}'),
                 ElevatedButton("test123", bgcolor="blue", color="white"),
             ]
         )
